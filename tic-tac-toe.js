fcd4921f54f034f3d724b7bee126d166999f2ab6
Justice <122542069+goob8079@users.noreply.github.com>
2025-06-27 14:19:50 +0900

    turned the check functions into IIFEs and moved them to displayWinner()

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -59,69 +59,63 @@ const Gameboard = (function() {
         return displayBoard();        
     }
 
-    // check to see if either X or O has 3 in a row
-    // private function
-    function checkRows() {
-        for (let i = 0; i < gameboardArr.length; i++) {
-            const row = gameboardArr[i];
-            const firstVal = row[0];
-
-            if (firstVal !== null && row.every(val => val === firstVal)) {
-                return firstVal;
-            }
-        }
-        // if there are no winning rows
-        return null;
-    }
-
-    function checkCols() {
-        let colArr = [];
-        // checks every column in gameboardArr
-        const arrCol = (arr, n) => arr.map(x => x[n]);
-
-        // push every column into colArr
-        // so first column will be colArr[0]
-        for (let i = 0; i < gameboardArr.length; i++) {
-            colArr.push(arrCol(gameboardArr, i));
-        }
-
-        // using the same logic as the checkRows() function
-        for (let j = 0; j < colArr.length; j++) {
-            const firstVal = colArr[j][0];
-            
-            // essentially checking every val in each "row"/index of colArr
-            // with the first value and seeing if they are the same
-            if (firstVal !== null && colArr[j].every(val => val === firstVal)) {
-                return firstVal;
-            }
-        }
-
-        return null;
-    }
-
-    function checkDiagonal() {
-        let downRight = gameboardArr.map((a, i) => a[i]);
-        // the ... makes it so the gameboardArr won't be changed with reverse()
-        let leftUp = [...gameboardArr].reverse().map((a, i) => a[i]);
-
-        const downRightFirstVal = downRight[0];
-        const leftUpFirstVal = leftUp[0];
-
-        if (downRightFirstVal !== null && downRight.every(val => val === downRightFirstVal)) {
-            return downRightFirstVal;
-        } 
-        
-        if (leftUpFirstVal !== null && leftUp.every(val => val === leftUpFirstVal)) {
-            return leftUpFirstVal;
-        }
-        
-        return null;
-    }
-
     function displayWinner() {
-        const rowWinner = checkRows();
-        const colWinner = checkCols();
-        const diagWinner = checkDiagonal();
+        const rowWinner =  (() => {
+            for (let i = 0; i < gameboardArr.length; i++) {
+                const row = gameboardArr[i];
+                const firstVal = row[0];
+
+                if (firstVal !== null && row.every(val => val === firstVal)) {
+                    return firstVal;
+                }
+            }
+            // if there are no winning rows
+            return null;
+        })();
+        
+        const colWinner = (() => {
+            let colArr = [];
+            // checks every column in gameboardArr
+            const arrCol = (arr, n) => arr.map(x => x[n]);
+
+            // push every column into colArr
+            // so first column will be colArr[0]
+            for (let i = 0; i < gameboardArr.length; i++) {
+                colArr.push(arrCol(gameboardArr, i));
+            }
+
+            // using the same logic as the checkRows() function
+            for (let j = 0; j < colArr.length; j++) {
+                const firstVal = colArr[j][0];
+                
+                // essentially checking every val in each "row"/index of colArr
+                // with the first value and seeing if they are the same
+                if (firstVal !== null && colArr[j].every(val => val === firstVal)) {
+                    return firstVal;
+                }
+            }
+
+            return null;
+        })();
+
+        const diagWinner = (() => {
+            let downRight = gameboardArr.map((a, i) => a[i]);
+            // the ... makes it so the gameboardArr won't be changed with reverse()
+            let leftUp = [...gameboardArr].reverse().map((a, i) => a[i]);
+
+            const downRightFirstVal = downRight[0];
+            const leftUpFirstVal = leftUp[0];
+
+            if (downRightFirstVal !== null && downRight.every(val => val === downRightFirstVal)) {
+                return downRightFirstVal;
+            } 
+            
+            if (leftUpFirstVal !== null && leftUp.every(val => val === leftUpFirstVal)) {
+                return leftUpFirstVal;
+            }
+            
+            return null;
+        })();
 
         const winner = rowWinner || colWinner || diagWinner;
         
@@ -153,11 +147,11 @@ const Gameboard = (function() {
 console.log(Gameboard.displayBoard());
 Gameboard.addPlayer("bob", "x");
 Gameboard.addPlayer("joe", "o");
-console.log(Gameboard.placeShape("X", 0, 0));
+console.log(Gameboard.placeShape("O", 0, 0));
 console.log(Gameboard.placeShape("O", 0, 1));
 console.log(Gameboard.placeShape("X", 0, 2));
 console.log(Gameboard.placeShape("O", 1, 0));
-console.log(Gameboard.placeShape("O", 1, 1));
+console.log(Gameboard.placeShape("X", 1, 1));
 console.log(Gameboard.placeShape("O", 1, 2));
 console.log(Gameboard.placeShape("X", 2, 0));
 console.log(Gameboard.placeShape("O", 2, 1));
